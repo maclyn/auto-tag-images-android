964d7c8cf56c976407b477284670043886b58ff1
Maclyn Brandwein <me@maclyn.me>
2014-10-18 18:38:12 -0400

    Rebuild for Kitkat
    Done

--- app/src/main/java/hacknyyo/org/autotagimages/MainActivity.java
+++ app/src/main/java/hacknyyo/org/autotagimages/MainActivity.java
@@ -18,10 +18,6 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements ActionBar.OnNavigationListener {
 
-    /**
-     * The serialization (saved instance state) Bundle key representing the
-     * current dropdown position.
-     */
     private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
 
     @Override
@@ -29,29 +25,24 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set up the action bar to show a dropdown list.
         final ActionBar actionBar = getActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 
-        // Set up the dropdown list navigation in the action bar.
         actionBar.setListNavigationCallbacks(
-                // Specify a SpinnerAdapter to populate the dropdown list.
                 new ArrayAdapter<String>(
                         actionBar.getThemedContext(),
                         android.R.layout.simple_list_item_1,
                         android.R.id.text1,
                         new String[] {
-                                getString(R.string.title_section1),
-                                getString(R.string.title_section2),
-                                getString(R.string.title_section3),
+                                "Tagged",
+                                "Untagged"
                         }),
                 this);
     }
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
-        // Restore the previously serialized current dropdown position.
         if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
             getActionBar().setSelectedNavigationItem(
                     savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
@@ -60,7 +51,6 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
-        // Serialize the current dropdown position.
         outState.putInt(STATE_SELECTED_NAVIGATION_ITEM,
                 getActionBar().getSelectedNavigationIndex());
     }
@@ -68,18 +58,14 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_tag_all) {
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -87,37 +73,25 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
 
     @Override
     public boolean onNavigationItemSelected(int position, long id) {
-        // When the given dropdown item is selected, show its contents in the
-        // container view.
-        getFragmentManager().beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
+        if(position == 0) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, TaggedFragment.newInstance())
+                    .commit();
+        } else {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, UntaggedFragment.newInstance())
+                    .commit();
+        }
         return true;
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
+    public static class TaggedFragment extends Fragment {
+        public static TaggedFragment newInstance() {
+            TaggedFragment fragment = new TaggedFragment();
             return fragment;
         }
 
-        public PlaceholderFragment() {
+        public TaggedFragment() {
         }
 
         @Override
@@ -128,4 +102,20 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
         }
     }
 
+    public static class UntaggedFragment extends Fragment {
+        public static UntaggedFragment newInstance() {
+            UntaggedFragment fragment = new UntaggedFragment();
+            return fragment;
+        }
+
+        public UntaggedFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
 }
