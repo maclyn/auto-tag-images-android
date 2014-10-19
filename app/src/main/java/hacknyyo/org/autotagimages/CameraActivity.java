4579a5ceb8f5097663b2aa62a7ecfd98fc19086d
Rushil Kumar <rukumar333@gmail.com>
2014-10-19 13:31:44 +0100

    Taking pictures now works

--- app/src/main/java/hacknyyo/org/autotagimages/CameraActivity.java
+++ app/src/main/java/hacknyyo/org/autotagimages/CameraActivity.java
@@ -1,21 +1,160 @@
 package hacknyyo.org.autotagimages;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.hardware.Camera;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.hardware.Camera.Parameters;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.List;
 
-public class CameraActivity extends Activity {
+import static android.view.View.resolveSize;
+
+public class CameraActivity extends Activity implements SurfaceHolder.Callback{
+
+    ImageButton cameraButton;
+    ImageButton smartButton;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    Camera camera;
+    List<Camera.Size> mSupportedPreviewSizes;
+    Camera.Size mPreviewSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+        FrameLayout layout = new FrameLayout(this);
+        Camera camera = Camera.open();
+        final CameraView view = new CameraView(this,camera);
+        layout.addView(view);
+        FrameLayout.LayoutParams flp1 = new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        flp1.gravity = Gravity.BOTTOM;
+        RelativeLayout buttonLayout = (RelativeLayout)getLayoutInflater().inflate(R.layout.camera_viewer, null);
+        layout.addView(buttonLayout,flp1);
+        setContentView(layout);
+        cameraButton = (ImageButton)findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.takeSmartPicture(true);
+                Toast.makeText(CameraActivity.this,"Took smart picture",Toast.LENGTH_LONG).show();
+            }
+        });
+        //FrameLayout layout = new FrameLayout(this);
+
+        //FrameLayout frameLayout = new FrameLayout(this);
+        //frameLayout.addView(view);
+
+        //RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.buttonlayout);
+        //frameLayout.addView(cameraButton);
+        /*
+        cameraButton = (ImageButton) findViewById(R.id.cameraButton);
+        smartButton = (ImageButton) findViewById(R.id.smartButton);
+
+        surfaceView = (SurfaceView) findViewById(R.id.cameraView);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        //Show_Preview();
+
+        /*
+        buttonStartCameraPreview.setOnClickListener( // write below code inside this block );
+                buttonStopCameraPreview.setOnClickListener( // write below code inside this block); }
+        */
     }
+
+    public void Show_Preview() {
+        camera = Camera.open();
+        List<Camera.Size> mSupportedPreviewSizes = camera.getParameters().getSupportedPreviewSizes();
+        try{
+            //camera.setPreviewCallback(this);
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        }catch(IOException e){
+
+        }
+        /*
+        if (!previewing) {
+            camera = Camera.open();
+            if (camera != null) {
+                try {
+                    camera.setPreviewDisplay(surfaceHolder);
+                    camera.startPreview();
+                    previewing = true;
+                } catch (IOException e) {
+// TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        */
+    }
+    /*
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        final int width = resolveSize(surfaceView.getMinimumHeight(), widthMeasureSpec);
+        final int height = resolveSize(surfaceView.getMinimumHeight(), heightMeasureSpec);
+        surfaceView.setMeasuredDimension(width, height);
+
+        if (mSupportedPreviewSizes != null) {
+            mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, width, height);
+        }
+    }
+    */
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio=(double)h / w;
+
+        if (sizes == null) return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+
+
 
     /**
      * Scans through a list of tags and searches for tags that correspond to certain scene modes.
@@ -39,5 +178,27 @@ public class CameraActivity extends Activity {
         }
         //TODO Implement setSceneMode after the rest of CameraActivity is created.
         //setSceneMode(scene);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        camera = Camera.open();
+        try{
+            //camera.setPreviewCallback(this);
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        }catch(IOException e){
+
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 }
---
+++ app/src/main/java/hacknyyo/org/autotagimages/PhotoHandler.java
@@ -0,0 +1,81 @@
+package hacknyyo.org.autotagimages;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.hardware.Camera;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by Rushil on 10/19/2014.
+ */
+public class PhotoHandler implements Camera.PictureCallback {
+    public static final String TAG = "PhotoHandler";
+
+    private final Context context;
+    private final Camera camera;
+
+    public PhotoHandler(Context context, Camera c) {
+        this.context = context;
+        this.camera = c;
+    }
+
+    @Override
+    public void onPictureTaken(byte[] bytes, Camera cam) {
+        Log.d(TAG, "Bytes received");
+        File photoDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        //Find photo directory within DCIM that contains "camera"
+        File subdirs[] = photoDirectory.listFiles();
+        File saveLocation = null;
+        for(File f : subdirs){
+            Log.d(TAG, "F: " + f.getPath());
+            if(f.getPath().toLowerCase(Locale.US).contains("camera")){
+                Log.d(TAG, "Are you set?");
+                saveLocation = f;
+            }
+            Log.d(TAG, "Save location: " + saveLocation);
+        }
+        if(saveLocation == null){
+            saveLocation = photoDirectory; //Fallback here if we don't have a "Camera" folder
+        }
+        photoDirectory = saveLocation;
+        if (!photoDirectory.exists() && !photoDirectory.mkdirs()) {
+            Toast.makeText(context, "R/W Error to photodir: " + photoDirectory.getPath(), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        int w = bmp.getWidth();
+        int h = bmp.getHeight();
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bmp, 0, 0, w, h, matrix, true);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+        String date = dateFormat.format(new Date());
+        String photoFile = "PHOTO_" + date + ".jpg";
+        String filename = photoDirectory.getPath() + File.separator + photoFile;
+        File outputFile = new File(filename);
+        try {
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            boolean result = rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 95, fos);
+            fos.close();
+            if (result) {
+                Toast.makeText(context, "Took photo", Toast.LENGTH_SHORT).show();
+            }
+            camera.startPreview();
+        } catch (Exception e) {
+            Toast.makeText(context, "Couldn't save image: " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}
