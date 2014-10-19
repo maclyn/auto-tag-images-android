ee8e7f168cb3a87d752704607a6192cfb998f4c4
Rushil Kumar <rukumar333@gmail.com>
2014-10-19 05:31:16 +0100

    Can access data from clarifai api successfully now

--- app/src/main/java/hacknyyo/org/autotagimages/CloudTag.java
+++ app/src/main/java/hacknyyo/org/autotagimages/CloudTag.java
@@ -8,6 +8,7 @@ import java.util.List;
  * Created by Rushil on 10/18/2014.
  */
 public class CloudTag {
+    /*
     public List<String> classes;
     public List<Double> probs;
 
@@ -17,5 +18,19 @@ public class CloudTag {
 
     public List<Double> getProbs(){
         return probs;
+    }
+    */
+    public String status_code;
+    public String status_msg;
+    public List<Result> results;
+
+    public String getStatus_code(){
+        return status_code;
+    }
+    public String getStatus_msg(){
+        return status_msg;
+    }
+    public List<Result> getResults(){
+        return results;
     }
 }
---
+++ app/src/main/java/hacknyyo/org/autotagimages/Result.java
@@ -0,0 +1,12 @@
+package hacknyyo.org.autotagimages;
+
+/**
+ * Created by Rushil on 10/18/2014.
+ */
+public class Result
+{
+    public String status_code;
+    public String status_message;
+    public String docid;
+    public InnerResult result;
+}
