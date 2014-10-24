f51b707277b084089155973bf0c0c786d7c5840c
Tim Hung <thung1@binghamton.edu>
2014-10-24 15:27:24 +0100

    Changed icon location to the icon I created in GIMP. New pretty icon.

--- app/src/main/java/hacknyyo/org/autotagimages/Constants.java
+++ app/src/main/java/hacknyyo/org/autotagimages/Constants.java
@@ -2,6 +2,6 @@ package hacknyyo.org.autotagimages;
 
 public class Constants {
     //Change these to your Clarifai client ID/client secret!
-    public static final String clientId = "your_client_id";
-    public static final String clientSecret = "your_client_secret";
+    public static final String clientId = "uXcLjdijZ1EyyV3aP320XFWTeZhJuBT0_RnqOWEn";
+    public static final String clientSecret = "jEnJLQ2tPHxrBODPGNYr-EssEO993wo-QKGjUgcw";
 }
